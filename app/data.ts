cf25f9ab02024b7d4abb637db88231259c435f27
HumanResourcesOutsourced Research <research@humanresourcesoutsourced.com>
2026-08-21 14:00:06 +0000

    Publish August 21 blog batch

--- app/data.ts
+++ app/data.ts
@@ -7,6 +7,7 @@ import { august18BlogPosts } from './blog/august18-batch';
 import { august18Repair4Posts } from './blog/august18-repair-4';
 import { august19BlogPosts } from './blog/august19-batch';
 import { august20BlogPosts } from './blog/august20-batch';
+import { august21BlogPosts } from './blog/august21-batch';
 
 export const site = {
   domain: 'HumanResourcesOutsourced.com',
@@ -126,7 +127,7 @@ const evergreenBlogPosts = [
 ] as const;
 
 // The dated daily batch is the newest family content and must precede evergreen posts.
-export const blogPosts = [...august20BlogPosts, ...august19BlogPosts, ...august18BlogPosts, ...august18Repair4Posts, ...august17BlogPosts, ...august14BlogPosts, ...august13BlogPosts, ...august11BlogPosts, ...dailyBlogPosts, ...evergreenBlogPosts] as const;
+export const blogPosts = [...august21BlogPosts, ...august20BlogPosts, ...august19BlogPosts, ...august18BlogPosts, ...august18Repair4Posts, ...august17BlogPosts, ...august14BlogPosts, ...august13BlogPosts, ...august11BlogPosts, ...dailyBlogPosts, ...evergreenBlogPosts] as const;
 
 export const staffingOffer = {
   included: [
